0d3d218ac4ce42daa2837f604532f7bbed551ab0
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2023-07-12 07:44:03 +0900

    chore: add description and another answer

--- 00000000_tessoku/a11.cpp
+++ 00000000_tessoku/a11.cpp
@@ -8,5 +8,14 @@ int main() {
   for (int i = 0; i < n; i++) cin >> a[i];
 
   int ans = lower_bound(a.begin(), a.end(), x) - a.begin() + 1;
+  // これでもOK
+  // int ans = ++lower_bound(a.begin(), a.end(), x) - a.begin();
   cout << ans << endl;
 }
+
+// lower_bound(a.begin(), a.end(), x) - a.begin()
+// イテレータ同士の引き算は、終端の1つ前の要素までの個数を返す
+// a.begin()は、先頭の要素を指すイテレータ
+// a.end()は、終端の次の要素を指すイテレータ
+// つまり、a.end() - a.begin()は、aの要素数となる
+// したがって、その要素数は添字と一致する
